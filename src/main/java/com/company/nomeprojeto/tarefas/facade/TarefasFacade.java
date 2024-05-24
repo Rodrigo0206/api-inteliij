4d99a405b752e78c945932a35e2cae58b11481e5
Rodrigo Domingues <91419413+Rodrigo0206@users.noreply.github.com>
2024-05-24 16:35:29 -0300

    fiz o metodo criar

---
+++ src/main/java/com/company/nomeprojeto/tarefas/facade/TarefasFacade.java
@@ -0,0 +1,20 @@
+package com.company.nomeprojeto.tarefas.facade;
+
+
+import com.company.nomeprojeto.tarefas.dto.TarefaDTO;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class TarefasFacade {
+    private static final Map<Long, TarefaDTO> tarefas = new HashMap<>();
+
+    public TarefaDTO criar(TarefaDTO tarefaDTO){
+        Long proximoId = tarefas.keySet().size() +1L;
+        tarefaDTO.setId(proximoId);
+        tarefas.put(proximoId, tarefaDTO );
+        return tarefaDTO;
+    }
+}
